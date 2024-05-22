b8bbf25a3ba4a6a85979ad87909b5a001b8d44ee
Talha <99talhayildiz@gmail.com>
2024-05-22 19:14:49 +0300

    Created Inbox Controller

---
+++ MailBox.PresentationLayer/Controllers/InboxController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MailBox.PresentationLayer.Controllers
+{
+    public class InboxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
