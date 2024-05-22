2b53bf6465e08f6be29b7073a3abc8b0e96946ad
Talha <99talhayildiz@gmail.com>
2024-05-22 17:09:35 +0300

    Created the Component Partial

---
+++ MailBox.PresentationLayer/ViewComponents/Member/_MemberLayoutHeaderPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MailBox.PresentationLayer.ViewComponents.Member
+{
+    public class _MemberLayoutHeaderPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        { 
+            return View(); 
+        }
+    }
+}
---
+++ MailBox.PresentationLayer/ViewComponents/Member/_MemberLayoutNavbarPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MailBox.PresentationLayer.ViewComponents.Member
+{
+    public class _MemberLayoutNavbarPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
